2f4c749bb807fdbc8d76d782f2d8fe1481a16cea
Novice Engineer <skyler32702@gmail.com>
2025-06-25 22:24:44 -0500

    Migration

---
+++ src/globals.ts
@@ -0,0 +1,9 @@
+export const homePage = "/NoviceEngineerStudio";
+export const blogPage = `${homePage}/blog`;
+export const untitledUniversePage = `${homePage}/untitled_universe`;
+
+export const navbarPages = [
+    { name: "Home", path: homePage },
+    { name: "Blog", path: blogPage },
+    { name: "Untitled Universe", path: untitledUniversePage },
+]
