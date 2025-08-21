80aa9937dcb6efaceeda39d3f6101d028694913d
Novice Engineer <skyler32702@gmail.com>
2025-08-21 12:49:54 -0500

    Web Service Page Navigation

---
+++ src/data/tiers/blog_links.ts
@@ -0,0 +1,19 @@
+import type { ServiceLink } from "../types/ServiceLink";
+
+const blog_links: ServiceLink[] = [
+    {
+        tag: "Blog Tier I",
+        tier: 1,
+        href: "/services/web/blog/tier-I",
+    }, {
+        tag: "Blog Tier II",
+        tier: 2,
+        href: "/services/web/blog/tier-II",
+    }, {
+        tag: "Blog Tier III",
+        tier: 3,
+        href: "/services/web/blog/tier-III",
+    },
+];
+
+export default blog_links;
---
+++ src/data/tiers/store_links.ts
@@ -0,0 +1,19 @@
+import type { ServiceLink } from "../types/ServiceLink";
+
+const store_links: ServiceLink[] = [
+    {
+        tag: "Store Tier I",
+        tier: 1,
+        href: "/services/web/store/tier-I",
+    }, {
+        tag: "Store Tier II",
+        tier: 2,
+        href: "/services/web/store/tier-II",
+    }, {
+        tag: "Store Tier III",
+        tier: 3,
+        href: "/services/web/store/tier-III",
+    },
+];
+
+export default store_links;
