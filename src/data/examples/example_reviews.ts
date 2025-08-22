ad3c856b49e774e9c21db6bc56332a872b6db1b4
Novice Engineer <skyler32702@gmail.com>
2025-08-22 10:08:37 -0500

    Tier I Landing Page Complete

--- src/data/examples/example_reviews.ts
+++ src/data/examples/example_reviews.ts
@@ -6,8 +6,14 @@ import example_review_2 from "../../assets/images/examples/example_review_2.webp
 const example_reviews: ReviewData[] = [
     {
         picture: example_review_1,
+        customer: "Tanya Mc'Nameson",
+        stars: 5,
+        description: "The pizza was amazing! Shipment took way less time than expected, and was well worth the price. Our family will be ordering again!",
     }, {
         picture: example_review_2,
+        customer: "Jake Jackson",
+        stars: 4,
+        description: "My husband and I are usually very picky eaters, but Lorenzo's pizza was tolerable. Great meal to spice things up between orders of chicken nuggets, french fries, and macoroni and cheese.",
     }
 ];
 
--- src/data/types/ReviewData.ts
+++ src/data/types/ReviewData.ts
@@ -1,5 +1,8 @@
 interface ReviewData {
     picture: ImageMetadata;
+    customer: string;
+    stars: number;
+    description: string;
 };
 
 export type { ReviewData };
